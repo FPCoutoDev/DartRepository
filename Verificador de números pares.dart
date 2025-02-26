981125d6c643fc0ab2cdc3af0cd25412bbc521dc
Miguel Couto <contatocoutomiguel@gmail.com>
2025-02-26 14:17:59 -0300

    Atualizei o arquivo Verificador de números pares, adicionando um mecanismo de ordem númerica crescente, contador de algarismo. Modifiquei o nome do mesmo.

--- Verificador de números pares.dart
+++ Verificador de números pares.dart
@@ -1,13 +1,14 @@
 void main(List<String> args) {
-  var lista = <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14];
-  lista.add(16);
-  lista.replaceRange(0, 0, [20]);
-  print(lista);
-  lista = [];
-  var b = 0;
-  for (var i = 0; i < lista.length; i++) {
-    final value = (lista[i] % 2 == 0) ? lista[i] : 0;
-    b += value;
-    print(b);
-  }
+  var listaDeNumeros = <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14];
+  listaDeNumeros.add(16);
+  listaDeNumeros.replaceRange(0, 0, [20]);
+  print("A lista inicial é $listaDeNumeros");
+  var listaPar = [];
+  for (var i = 0; i < listaDeNumeros.length; i++) {
+    if (listaDeNumeros[i] % 2 ==0){
+   listaPar.add(listaDeNumeros[i]);
+    }
+   }
+  listaPar.sort((a, b) => a.compareTo(b));
+  print("A lista filtrada com apenas algarismo pares tem ${listaPar.length} números: ${listaPar}");
 }
